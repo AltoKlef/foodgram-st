b527f7475cfd8dc8312efd6af1cfe6cb1870f3eb
Ruslan <garridubua@gmail.com>
2025-06-03 17:23:24 +0300

    fix images

--- backend/recipes/pagination.py
+++ backend/recipes/pagination.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 
 
 class RecipesPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 6
 
     def get_paginated_response(self, data):
         return Response(data)
